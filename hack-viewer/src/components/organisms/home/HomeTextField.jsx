9e111b9659f7e875289f14dda215833870ba76d6
Nameless <tanisyun0331@icloud.com>
2022-12-10 18:39:44 +0900

    api 通信用の基盤実装

--- hack-viewer/src/components/organisms/home/HomeTextField.jsx
+++ hack-viewer/src/components/organisms/home/HomeTextField.jsx
@@ -1,19 +1,32 @@
 import { Button, TextField, Typography } from "@mui/material";
 import { Box } from "@mui/system";
+import { useState } from "react";
+import axios from "../../../utils/http_client";
 
 const HomeTextField = () => {
+    const [eventId, setEventId] = useState()
+    const [event, setEvent] = useState()
+  
+    const getEvent = ()=>{
+      axios.get(`/api/events/${eventId}`).then(req =>{
+        setEvent(req.body)
+      }).catch(res => {
+        console.log(res);
+      })
+    }
     return(
         <Box>
             <Typography variant="body2" gutterBottom>
                 イベントへ参加
             </Typography>
-            <TextField sx={{mt:1}} id="eventid" label="イベントID"></TextField>
+            <TextField sx={{mt:1}} id="eventid" label="イベントID" value={eventId} onChange={e => setEventId(e.target.value)}></TextField>
             <Box>
                 <Button
                     noValidate
                     autoComplete="off"
                     variant="contained"
                     sx={{mt:2}}
+                    onClick={getEvent}
                 >
                     送信
                 </Button>
