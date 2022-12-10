219acdfb1c003f67219556e768f517e8f805d2bc
Yuma Satake <satous.y@icloud.com>
2022-12-10 12:42:59 +0900

    コンポーネント名ミス修正

--- hack-viewer/src/App.js
+++ hack-viewer/src/App.js
@@ -1,7 +1,7 @@
 import { CssBaseline } from "@mui/material";
 import EventPage from "./components/pages/EventPage";
 import HomePage from "./components/pages/HomePage";
-import TeamSettingPage from "./components/pages/TeamSetting";
+import SettingPage from "./components/pages/TeamSetting";
 
 function App() {
   return (
@@ -9,7 +9,7 @@ function App() {
       <CssBaseline></CssBaseline>
       <HomePage/>
       <EventPage/>
-      <TeamSettingPage/>
+      <SettingPage/>
     </>
   );
 }
