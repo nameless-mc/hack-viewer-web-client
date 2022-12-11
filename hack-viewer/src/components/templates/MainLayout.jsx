de2ba56ff3289e983e8b2ea9f27e0d6ae0c69685
Nameless <tanisyun0331@icloud.com>
2022-12-11 14:51:06 +0900

    表示の調整

--- hack-viewer/src/components/templates/MainLayout.jsx
+++ hack-viewer/src/components/templates/MainLayout.jsx
@@ -1,16 +1,17 @@
+import { Container } from "@mui/material";
 import { Box } from "@mui/system";
 import MainAppBar from "../organisms/MainAppBar";
 
 const MainLayout = (props) => {
-    const { children } = props;
-    return(
-        <>
-            <MainAppBar/>
-            <Box sx={{m:6}}>
-                {children}
-            </Box>
-        </>
-    )
-}
+  const { children } = props;
+  return (
+    <>
+      <MainAppBar />
+      <Container>
+        <Box sx={{ m: 6 }}>{children}</Box>
+      </Container>
+    </>
+  );
+};
 
 export default MainLayout;
