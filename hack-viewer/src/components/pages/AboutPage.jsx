e4ca35a9832abd57665587791d78f0762f1e0923
Kouki2022 <shi671984@gmail.com>
2022-12-11 12:07:49 +0900

    レヴュー修正

--- hack-viewer/src/components/pages/AboutPage.jsx
+++ hack-viewer/src/components/pages/AboutPage.jsx
@@ -1,8 +1,9 @@
 import { Box } from "@mui/material";
-import { Button, FormControl, FormControlLabel, List, ListItem, ListItemText, Radio, RadioGroup, TextField, Typography } from "@mui/material";
+import { List, ListItem, ListItemText, Typography } from "@mui/material";
 import React from 'react'
 import MainLayout from "../templates/MainLayout";
 import MainTitle from "../organisms/MainTitle";
+
 
 const AboutPage=()=> {
   return(
@@ -13,49 +14,55 @@ const AboutPage=()=> {
             <list>
                 <listItem>
                     <listItemtText>
-                        <Typography  style={{ color: 'orange' }}>
-                            <Box sx={{ fontWeight:'bold',fontSize: 30, mt: 7,mb:2}}>
-                                1.目的
-                            </Box>
-                        </Typography>
+                    <Typography  style={{ color: 'orange' }}>
+                        <Box sx={{ fontWeight:'bold',fontSize: 30, mt: 7,mb:2}}>
+                            1.目的
+                        </Box>
+                    </Typography>
                     </listItemtText>
                     <listItemtText>
                         <Typography Box sx={{fontWeight:'bold',ml:4}}>
                             このサービスはハッカソン初心者が参加した際に感じるであろう
-                            <Typography style={{ color: 'red' }}Box sx={{ fontWeight:'bold',fontSize: 20, my: 2}}>
-                                「他のチームはどのくらい進んでいるのか」
+                            <Typography style={{ color: 'red' }}Box sx={{ fontWeight:'bold',fontSize: 20, my: 1}}>
+                                「自分のチームは遅れているのではないか」
                             </Typography>
-                            という不安を解消することを目的としたサービスです。<br/><br/>
-                            また、イベント主催者は各チームの進捗状況を確認することにより<br/>
+                            という不安を他チームの進捗を「見える化」することで解消することを目的としたサービスです。<br/>
+
+                            また、イベント管理者は各チームの進捗状況を確認することにより<br/>
                             フォローやスケジュール管理がしやすくなります。                  
                         </Typography>
                     </listItemtText>
                 </listItem>
 
-                <listItemt>
+                <List>
+                <ListItem>
                     <ListItemText >
                         <Typography style={{ color: 'orange' }} Box sx={{fontWeight:'bold',fontSize:30,mt:7,mb:2}}>
                             2.できること
                         </Typography>
+
                         <Typography Box sx={{fontWeight:'bold',ml:4}}>
+                        <Typography style={{color:'red'}} Box sx={{ fontWeight:'bold',fontSize: 20, mt: 5,mb:2}}>
+                            参加者の方
+                        </Typography>
                             ・自チームの進捗確認<br/>
                             ・他チームの進捗確認<br/>
-                            ・チーム名変更<br/>
+                            ・チーム名変更
                         </Typography>
                     </ListItemText>
-                </listItemt>
 
-                <listItem>
-                    <listItemText>
-                        <Typography style={{ color: 'orange' }} Box sx={{fontSize:30,mt:7}}>
-                            3.開発チーム
+                    <ListItemText>
+                        <Typography Box sx={{fontWeight:'bold',ml:4}}>
+                        <Typography style={{color:'red'}} Box sx={{ fontWeight:'bold',fontSize: 20, mt: 18,mb:2}}>
+                            イベント管理者の方
                         </Typography>
-                        <Typography Box sx={{fontWeight:'bold',fontSize:20,ml:4}}>
-                            チーム名:28時間マラソン（446）
+                            ・チームの追加・編集・削除<br/>
+                            ・イベント名の編集・削除<br/>
+                            ・イベント概要の編集・削除
                         </Typography>
-                    </listItemText>
-                </listItem>
-
+                    </ListItemText>
+                    </ListItem>
+                </List>
             </list>
         </Box>
     </MainLayout>
--- hack-viewer/src/components/pages/HowtoPage.jsx
+++ hack-viewer/src/components/pages/HowtoPage.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Box } from "@mui/material";
-import { Button, FormControl, FormControlLabel, List, ListItem, ListItemText, Radio, RadioGroup, TextField, Typography } from "@mui/material";
+import { Typography } from "@mui/material";
 import MainLayout from "../templates/MainLayout";
 import MainTitle from "../organisms/MainTitle";
 
@@ -13,7 +13,7 @@ const HowtoPage = () => {
                 <list>
                     <listItem>
                         <Typography style={{color:'red'}} Box sx={{ fontWeight:'bold',fontSize: 30, mt: 7,mb:2}}>
-                            For participant
+                            参加者の方へ
                         </Typography>
                         <Typography Box sx={{fontWeight:'bold',fontSize:20,ml:3,my:5}}>
                             <Box sx={{color:'orange'}}>
@@ -25,30 +25,29 @@ const HowtoPage = () => {
                                 (3)イベントページに遷移
                             </Box>
                         </Typography>
-
                         <Typography Box sx={{fontWeight:'bold',fontSize:20,ml:3,my:5}}>
                             <Box style={{color:'orange'}}>
                                 2.進捗確認更新
                             </Box>
                             <Box sx={{fontSize:17,ml:7,mt:2}}>
-                            (1)イベントページで自チームの「進捗をPUSHするボタン」を押す<br/>
+                            (1)イベントページで自チームの「進捗をPUSHする」ボタンを押す<br/>
                             (2)チーム管理ページに遷移<br/>
                             (3)進捗ラジオボタンを進める<br/>
                             (4)送信ボタンを押す
                             </Box>
                         </Typography>
-
+                        
                         <Typography style={{color:'red'}} Box sx={{fontWeight:'bold',fontSize: 30, mt: 7,mb:2}}>
-                            For Host
+                            イベント管理者の方へ
                         </Typography>
                         <Typography style={{color:''}} Box sx={{fontWeight:'bold',fontSize:20,ml:3,my:3}}>
                             1.イベントページ最下部より管理者ページへアクセス<br/>
                             2.下記の編集が行えます
                         </Typography>
-                            <Box sx={{fontSize:17,ml:7,mt:2}}>
-                                (1)ホーム画面でイベントID入力<br/>
-                                (2)送信ボタンを押す<br/>
-                                (3)イベントページに遷移
+                            <Box sx={{fontWeight:'bold',Size:17,ml:7,mt:2}}>
+                                (1)チームの追加・編集・削除<br/>
+                                (2)イベント名の編集・削除<br/>
+                                (3)イベント概要の編集・削除
                             </Box>
                         
                     </listItem>
