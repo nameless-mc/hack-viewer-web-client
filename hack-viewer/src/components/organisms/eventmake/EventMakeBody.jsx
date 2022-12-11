72dfab59594edcf0f212ae5a93d9b6790e26b196
Nameless <tanisyun0331@icloud.com>
2022-12-11 14:50:50 +0900

    イベント作成ページ作成（APIなし）

--- hack-viewer/src/components/organisms/eventmake/EventMakeBody.jsx
+++ hack-viewer/src/components/organisms/eventmake/EventMakeBody.jsx
@@ -1,61 +1,106 @@
-import { Button, Container, Grid, List, ListItem, ListItemText, Slider, TextField, Typography } from "@mui/material";
+import {
+  Button,
+  Container,
+  Divider,
+  Grid,
+  List,
+  ListItem,
+  ListItemText,
+  Slider,
+  Stack,
+  TextField,
+  Typography,
+} from "@mui/material";
 import Grid2 from "@mui/material/Unstable_Grid2/Grid2";
 import { Box } from "@mui/system";
 import { useState } from "react";
 import EventMakeCard from "./EventMakeCard";
 
+const Content = (props) => {
+  const { children } = props;
+  return <Box paddingLeft={5}>{children}</Box>;
+};
 
 const EventMakeBody = () => {
-    const [teams, setTeams] = useState(1)
+  const [teams, setTeams] = useState([{ id: 0, name: "" }]);
+  const [currentId, setCurrentId] = useState(0);
 
-    const plusTeams = () => setTeams(teams+1);
-    const minusTeams = () => setTeams(teams-1);
+  const getNextId = () => {
+    const next = currentId + 1;
+    setCurrentId(next);
+    return next;
+  };
 
-    return(
+  const addTeam = () => {
+    setTeams([...teams, { name: "", id: getNextId() }]);
+  };
+
+  const deleteTeam = (team) => {
+    const newTeams = teams.filter((t) => t !== team);
+    setTeams(newTeams);
+  };
+
+  return (
+    <Stack spacing={2}>
+      <Content>
+        <ListItemText>
+          <Typography variant="subtitle1">イベント名</Typography>
+        </ListItemText>
+        <TextField
+          label="イベント名"
+          variant="outlined"
+          sx={{ width: 300 }}
+        ></TextField>
+      </Content>
+      <Content>
+        <ListItemText>
+          <Typography variant="subtitle1" sx={{ py: "auto" }}>
+            イベント概要
+          </Typography>
+        </ListItemText>
+        <TextField
+          label="イベント概要"
+          variant="outlined"
+          multiline
+          rows={4}
+          sx={{ width: 300 }}
+        ></TextField>
+      </Content>
+      <Content>
+        <ListItemText>
+          <Typography variant="subtitle1">管理者パスワード</Typography>
+        </ListItemText>
+        <TextField
+          label="管理者パスワード"
+          variant="outlined"
+          sx={{ width: 300 }}
+        ></TextField>
+      </Content>
+      <Content>
         <Box>
-            <List>
-                <ListItem divider sx={{py:2}}>
-                    <ListItemText>
-                        <Typography variant="subtitle1">
-                            イベント名
-                        </Typography>
-                    </ListItemText>
-                    <TextField label="イベント名" variant="outlined" sx={{width:300}}></TextField>
-                </ListItem>
-                <ListItem divider sx={{py:2, height:200}}>
-                    <ListItemText>
-                        <Typography variant="subtitle1" sx={{py:'auto'}}>
-                            イベント概要
-                        </Typography>
-                    </ListItemText>
-                    <TextField label="イベント概要" variant="outlined" multiline rows={4} sx={{width:300}}></TextField>
-                </ListItem>
-                <ListItem divider sx={{py:2}}>
-                    <ListItemText>
-                        <Typography variant="subtitle1">
-                            管理者パスワード
-                        </Typography>
-                    </ListItemText>
-                    <TextField label="管理者パスワード" variant="outlined" sx={{width:300}}></TextField>
-                </ListItem>
-                <ListItem divider sx={{py:5}}>
-                    <ListItemText>
-                        <Typography variant="subtitle1">
-                            チーム数
-                        </Typography>
-                    </ListItemText>
-                    <Button sx={{mx:3}} variant="contained" onClick={plusTeams}>追加</Button>
-                    <Button variant="contained" onClick={minusTeams}>削除</Button>
-                </ListItem>
-            </List>
-            {[...Array({teams})].map((_) => (
-                <Grid item key={teams}>
-                    <EventMakeCard></EventMakeCard>
-                </Grid>
-                )
-            )}
+          <Typography variant="subtitle1">チーム</Typography>
         </Box>
-    )
-}
+        {teams.map((team) => (
+          <EventMakeCard
+            name={team.name}
+            setName={(name) => setTeams({ ...team, name: name })}
+            deleteTeam={() => deleteTeam(team)}
+            key={team.id}
+          />
+        ))}
+        <Button sx={{ mx: 3 }} variant="contained" onClick={addTeam}>
+          追加
+        </Button>
+      </Content>
+      <Content>
+        <Box display={"flex"} justifyContent={"flex-end"}>
+          <Button sx={{ mx: 3 }} variant="contained">
+            作成
+          </Button>
+        </Box>
+      </Content>
+    </Stack>
+  );
+};
 
 export default EventMakeBody;
--- hack-viewer/src/components/organisms/eventmake/EventMakeCard.jsx
+++ hack-viewer/src/components/organisms/eventmake/EventMakeCard.jsx
@@ -1,31 +1,44 @@
-import * as React from 'react';
-import Box from '@mui/material/Box';
-import Card from '@mui/material/Card';
-import CardContent from '@mui/material/CardContent';
-import Typography from '@mui/material/Typography';
-import { Button, CardActions, Step, StepLabel, Stepper, TextField } from '@mui/material';
+import * as React from "react";
+import Box from "@mui/material/Box";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import Typography from "@mui/material/Typography";
+import {
+  Button,
+  CardActions,
+  Step,
+  StepLabel,
+  Stepper,
+  TextField,
+} from "@mui/material";
 
 import teamData from "./teamData.json";
+import { Stack } from "@mui/system";
 
-const steps = [
-    "アイデア出し",
-    "仕様検討",
-    "機能開発",
-    "統合",
-    "プレゼン作成"
-];
-
-function EventMakeCard() {
+const EventMakeCard = (props) => {
+  const {name, setName, deleteTeam} = props
   return (
-    <Card sx={{width:340, m:3}}>
-        <CardContent>
-            <Typography sx={{ fontSize: 14 }} color="text.secondary">
-                チーム名
-            </Typography>
-            <TextField label="チーム名" variant="outlined" sx={{width:300}}></TextField>
-        </CardContent>
+    <Card sx={{ width: 340, m: 3 }}>
+      <CardContent>
+        <Stack
+          direction="row"
+          spacing={2}
+          display={"flex"}
+          alignItems={"center"}
+          justifyContent={"center"}
+        >
+          <TextField
+            label="チーム名"
+            variant="outlined"
+            sx={{ width: 300 }}
+          ></TextField>
+          <Box>
+            <Button variant="contained" onClick={deleteTeam}>削除</Button>
+          </Box>
+        </Stack>
+      </CardContent>
     </Card>
   );
-}
+};
 
 export default EventMakeCard;
