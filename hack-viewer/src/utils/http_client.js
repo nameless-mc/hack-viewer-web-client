9e35b8e5d2bce5e88dbf0eb2b028f8a34baa567f
Nameless <tanisyun0331@icloud.com>
2022-12-11 13:47:50 +0900

    チーム設定画面の実装

--- hack-viewer/src/utils/http_client.js
+++ hack-viewer/src/utils/http_client.js
@@ -4,9 +4,20 @@ const axios = new Axios({
   baseURL: process.env.REACT_APP_API_BASE_URL || "http://localhost:8081",
   withCredentials: true,
   responseType: "json",
-  transformResponse: [function transformResponse(data, headers) {
-    return JSON.parse(data);
-  }]
+  transformResponse: [
+    function transformResponse(data, headers) {
+      return JSON.parse(data);
+    },
+  ],
+  transformRequest: [
+    function (data, headers) {
+      return JSON.stringify(data);
+    },
+  ],
+  validateStatus: function (status) {
+    return status < 400;
+  },
+  headers: { "Content-Type": "application/json" },
 });
 
 export default axios;
